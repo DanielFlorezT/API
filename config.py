9d302caab3ed1914a83cde6026319a91ba64a08c
Daniel Florez Thomas <69448614+DanielFlorezT@users.noreply.github.com>
2024-11-26 17:28:26 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-from pydantic_settings import BaseSettings  # Cambiado para soportar pydantic 2.x
+from pydantic_settings import BaseSettings
 from pydantic import AnyHttpUrl
 from typing import List
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
-        "http://localhost",  # Cambia esto según sea necesario
+        "http://localhost",
         "http://127.0.0.1",
     ]
     PROJECT_NAME: str = "API de Proyecto Final"
@@ -16,5 +16,6 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
 
 
